03e7d26f8bf59b03e89776482f24a96ba74f8209
bro <bro@thinkpad.fritz.box>
2026-08-17 17:10:45 +0200

    feat: add CLI entrypoint

--- cmd/devvsite/main.go
+++ cmd/devvsite/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const version = "0.1.0"
 
 func main() {
-    fmt.Println("devvsite")
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		printHelp()
+		return
+	}
+
+	switch args[0] {
+	case "version":
+		fmt.Println("devvsite", version)
+
+	case "help", "--help", "-h":
+		printHelp()
+
+	default:
+		fmt.Printf("Unknown command: %s\n", args[0])
+		fmt.Println("Run 'devvsite help' for usage.")
+	}
+}
+
+func printHelp() {
+	fmt.Println("devvsite - PHP development environment CLI")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println("  devvsite <command>")
+	fmt.Println()
+	fmt.Println("Commands:")
+	fmt.Println("  help       Show this help message")
+	fmt.Println("  version    Show devvsite version")
 }
